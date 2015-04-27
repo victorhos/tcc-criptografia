eedfac78c938f5decc6c6545a6f283028b13cceb
Victor Hugo <victor.hos@gmail.com>
2015-04-27 20:03:05 -0300

    class pixel

--- sistema/src/main/java/br/com/ibta/tcc/main/Starter.java
+++ sistema/src/main/java/br/com/ibta/tcc/main/Starter.java
@@ -11,37 +11,36 @@ import javax.imageio.ImageIO;
 public class Starter {
 
 	public static void main(String[] args) {
-		
-		
+
 		BufferedImage img = null;
-		
+
 		try {
 			img = ImageIO.read(new File("/Users/victor/Pictures/image2px.jpg"));
 		} catch (IOException e) {
-			//System.out.println("Erro ao carregar a img");
+			// System.out.println("Erro ao carregar a img");
 		}
-	
-		/*
+
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
 		try {
 			ImageIO.write(img, "jpg", baos);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		*/
-		int q = img.getRGB(0, 0, img.getWidth(), img.getHeight(), null, 0, img.getWidth())[0];
-		int b = convert(q);
-		
-		
-		//COR
+
+		Integer q = img.getRGB(0, 0, img.getWidth(), img.getHeight(), null, 0,
+				img.getWidth())[0];
+		Integer b = convert(q);
+
+		System.out.println(">>>>>>>>>>>" + q.getClass());
+
+		// COR
 		System.out.println(q);
-		
-		
+
 		System.out.println(b);
-		
+
 		System.out.println(Integer.toHexString(0));
-		
+
 		Color color = new Color(q);
 		int red = color.getRed();
 		int green = color.getGreen();
@@ -51,13 +50,11 @@ public class Starter {
 		System.out.println(Integer.toHexString(red));
 		System.out.println(Integer.toBinaryString(green));
 		System.out.println(blue);
-	
+
 	}
 
 	public static int convert(int n) {
 		return Integer.valueOf(String.valueOf(n), 16);
 	}
-	
-	
 
 }
