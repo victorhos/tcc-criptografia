e60bb474c81457036c119104dc1d1754ca83cd42
Victor Hugo <victor.hos@gmail.com>
2015-04-28 17:50:12 -0300

    function create image

--- sistema/src/main/java/br/com/ibta/tcc/main/Image.java
+++ sistema/src/main/java/br/com/ibta/tcc/main/Image.java
@@ -9,35 +9,43 @@ import javax.imageio.ImageIO;
 public class Image {
 
 	private BufferedImage image;
-	private BufferedImage generated_image;
-	private String path_image;
+	private BufferedImage generatedImage;
+	private String pathImage;
+	private String generatedPathImage;
 
 	public Image(String path) {
 
-		this.path_image = path;
+		this.pathImage = path;
 
 	}
 
 	public void loadImage() {
 
 		try {
-			setImage(ImageIO.read(new File(getPath_image())));
+			setImage(ImageIO.read(new File(getPathImage())));
+			System.out.println("----------------------------------");
+			System.out.println("Imagem carregada com sucesso");
 		} catch (IOException e) {
-			System.out.println("Load img failed");
+			System.out.println("----------------------------------");
+			System.out.println("Erro ao carregar imagem");
 			System.out.println(e.getLocalizedMessage());
 		}
 
 	}
-	
-	public void createImage(){
-		
-		setGenerated_image(new BufferedImage(
-				getImage().getWidth(),
-				getImage().getHeight(), 
-				BufferedImage.TYPE_INT_RGB));
-		
-		
-		
+
+	public void createImage() throws IOException {
+
+		setGeneratedImage(new BufferedImage(getImage().getWidth(), getImage()
+				.getHeight(), BufferedImage.TYPE_INT_RGB));
+
+		File imageFile = new File(getPathImage());
+
+		ImageIO.write(getGeneratedImage(), "png", imageFile);
+
+		System.out.println("----------------------------------");
+		System.out.println("Imagem criada em:");
+		System.out.println(imageFile.getPath());
+
 	}
 
 	public int[] listRGB() {
@@ -55,24 +63,32 @@ public class Image {
 		return image;
 	}
 
-	public void setImage(BufferedImage img) {
-		this.image = img;
+	public void setImage(BufferedImage image) {
+		this.image = image;
 	}
 
-	public BufferedImage getGenerated_image() {
-		return generated_image;
+	public BufferedImage getGeneratedImage() {
+		return generatedImage;
 	}
 
-	public void setGenerated_image(BufferedImage generated_image) {
-		this.generated_image = generated_image;
-	}
-	
-	public String getPath_image() {
-		return path_image;
+	public void setGeneratedImage(BufferedImage generatedImage) {
+		this.generatedImage = generatedImage;
 	}
 
-	public void setPath_image(String path_image) {
-		this.path_image = path_image;
+	public String getPathImage() {
+		return pathImage;
+	}
+
+	public void setPathImage(String pathImage) {
+		this.pathImage = pathImage;
+	}
+
+	public String getGeneratedPathImage() {
+		return generatedPathImage;
+	}
+
+	public void setGeneratedPathImage(String generatedPathImage) {
+		this.generatedPathImage = generatedPathImage;
 	}
 
 }
